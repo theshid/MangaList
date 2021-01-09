7c874049202337dd0bc0b34196c77659d3b8e594
Shid Insa <theshid.insa@gmail.com>
2021-01-09 04:24:48 +0100

    implemented network module

--- app/src/main/java/com/shid/mangalist/data/di/NetworkModule.kt
+++ app/src/main/java/com/shid/mangalist/data/di/NetworkModule.kt
@@ -1,4 +1,49 @@
 package com.shid.mangalist.data.di
 
+import com.shid.mangalist.data.remote.network.ApiServices
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val BASE_URL = "https://api.jikan.moe/v3/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() =  OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient,moshi: Moshi): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiServices(retrofit: Retrofit):ApiServices = retrofit.create(ApiServices::class.java)
 }
---
+++ app/src/main/java/com/shid/mangalist/data/remote/network/ApiServices.kt
@@ -0,0 +1,35 @@
+package com.shid.mangalist.data.remote.network
+
+import com.shid.mangalist.data.remote.response.detail.CharactersResponse
+import com.shid.mangalist.data.remote.response.detail.DetailAnimeResponse
+import com.shid.mangalist.data.remote.response.detail.VideosResponse
+import com.shid.mangalist.data.remote.response.search.SearchAnimeResponse
+import com.shid.mangalist.data.remote.response.season.SeasonAnimeRespon
+import com.shid.mangalist.data.remote.response.top.TopAnimeResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiServices {
+
+    @GET("top/anime/1/{type}")
+    suspend fun getTopAnime(@Path("type") type: String): TopAnimeResponse
+
+    @GET("anime/{mal_id}")
+    suspend fun getDetailAnime(@Path("mal_id") mal_id: Int): DetailAnimeResponse
+
+    @GET("season/{year}/{season}")
+    suspend fun getSeasonAnime(
+        @Path("year") seasonYear: Int,
+        @Path("season") seasonName: String
+    ): SeasonAnimeRespon
+
+    @GET("search/anime?&page=1")
+    suspend fun getSearchAnime(@Query("q") query: String): SearchAnimeResponse
+
+    @GET("anime/{id}/characters_staff")
+    suspend fun getCharacters(@Path("id") id: Int): CharactersResponse
+
+    @GET("anime/{id}/videos")
+    suspend fun getVideos(@Path("id") id: Int): VideosResponse
+}
